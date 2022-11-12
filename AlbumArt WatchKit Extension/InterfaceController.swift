71dec9710d354ec20ad84a721b64e1dc0d8b0fdb
Ben Sauberman <bsauberman@gmail.com>
2022-11-12 16:17:13 -0700

    Initial Commit

---
+++ AlbumArt WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  AlbumArt WatchKit Extension
+//
+//  Created by Ben Sauberman on 11/12/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
